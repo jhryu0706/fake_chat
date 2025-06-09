e07cdd9c67ece297aad8f85b8729e30f0ddea0f1
Irene Ryu <jhryu@uchicago.edu>
2025-06-09 10:35:16 -0400

    12 creating agents data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,13 +4,19 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "../components/data-table";
+import { columns } from "@/modules/agents/ui/components/columns";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   // Data can never be null when using useSuspenseQuery. Therefore no need to rely on isLoading or isError
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+    </div>
+  );
 };
 
 export const AgentsLoadingView = () => {
