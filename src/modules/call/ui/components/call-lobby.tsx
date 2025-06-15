b9fe439a1c5ab41ab334dff1ac3a9592512a6005
Irene Ryu <jhryu@uchicago.edu>
2025-06-15 16:18:39 -0400

    22 setting up video call via stream.io

---
+++ src/modules/call/ui/components/call-lobby.tsx
@@ -0,0 +1,85 @@
+import { LogInIcon } from "lucide-react";
+import {
+  DefaultVideoPlaceholder,
+  StreamVideoParticipant,
+  ToggleAudioPreviewButton,
+  ToggleVideoPreviewButton,
+  useCallStateHooks,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
+import { authClient } from "@/lib/auth-client";
+import { generateAvatarUri } from "@/lib/avatar";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  onJoin: () => void;
+}
+
+const DisabledVideoPreview = () => {
+  const { data } = authClient.useSession();
+
+  return (
+    <DefaultVideoPlaceholder
+      participant={
+        {
+          name: data?.user.name ?? "",
+          image:
+            data?.user.image ??
+            generateAvatarUri({
+              seed: data?.user.name ?? "",
+              variant: "initials",
+            }),
+        } as StreamVideoParticipant
+      }
+    />
+  );
+};
+
+const AllowBrowserPermission = () => {
+  return (
+    <p className="text-sm">
+      Please grant your browser permission to access your camera and microphone.
+    </p>
+  );
+};
+
+export const CallLobby = ({ onJoin }: Props) => {
+  const { useCameraState, useMicrophoneState } = useCallStateHooks();
+  const { hasBrowserPermission: hasMicPermission } = useMicrophoneState();
+  const { hasBrowserPermission: hasCameraPermission } = useCameraState();
+
+  const hasBrowserMediaPermission = hasCameraPermission && hasMicPermission;
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full bg-radial from-sidebar-accent to-sidebar">
+      <div className="py-4 px-8 flex flex-1 items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
+          <h6 className="text-lg font-medium">Ready to join?</h6>
+          <p className="text-sm">Set up your call before joining</p>
+          <VideoPreview
+            DisabledVideoPreview={
+              hasBrowserMediaPermission
+                ? DisabledVideoPreview
+                : AllowBrowserPermission
+            }
+          />
+          <div className="flex gap-x-2">
+            <ToggleAudioPreviewButton />
+            <ToggleVideoPreviewButton />
+          </div>
+          <div className="flex gap-x-2 justify-between w-full">
+            <Button>
+              <Link href="/meetings">Cancel</Link>
+            </Button>
+            <Button onClick={onJoin}>
+              <LogInIcon />
+              Join Call
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
